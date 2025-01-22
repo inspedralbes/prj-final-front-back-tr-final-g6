657c0e445456dcd6e63a898bf31790d61b9f1cfc
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2025-01-22 13:13:44 +0100

    creada landing

---
+++ front/nuxt/pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 class="text-3xl font-bold text-gray-800">Iniciar Sesión</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped></style>
