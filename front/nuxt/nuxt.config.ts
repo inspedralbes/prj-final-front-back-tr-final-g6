454246c204be4666414d1bdfb276c69513dd3767
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2025-01-23 10:17:55 +0100

    pagina login + landing done

--- front/nuxt/nuxt.config.ts
+++ front/nuxt/nuxt.config.ts
@@ -1,9 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  // plugins: ["~/plugins/primevue.js"],
   modules: [
     '@primevue/nuxt-module',
+    '@nuxtjs/tailwindcss',
   ],
-})
+  css: [
+    "primevue/resources/primevue.min.css", // Estilos básicos de PrimeVue
+    "primeicons/primeicons.css", // Iconos de PrimeVue
+    "tailwindcss/tailwind.css" // TailwindCSS
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
+});
--- front/nuxt/pages/index.vue
+++ front/nuxt/pages/index.vue
@@ -1,19 +1,44 @@
 <template>
-    <div>
-        <h1>Bienvenido a mi proyecto de Nuxt</h1>
-        <p>Esta es la página de inicio.</p>
-        <Button label="Iniciar Sessió" />
+    <div class="min-h-screen bg-gradient-to-r from-[#07C8F9] via-[#0A85ED] to-[#0D41E1] flex flex-col items-center justify-center animated-bg">
+        <div class="text-center text-white px-6">
+            <h1 class="text-5xl font-extrabold mb-4">
+                Bienvenido a mi proyecto de Nuxt
+            </h1>
+            <p class="text-lg mb-6">
+                Este es un proyecto desarrollado con Nuxt, TailwindCSS y PrimeVue. ¡Explora las funcionalidades!
+            </p>
+            <nuxt-link to="/login">
+                <Button label="Inicia Sessió" icon="pi pi-user" class="bg-indigo-800 text-white font-bold py-2 px-4 rounded border border-black" />
+            </nuxt-link>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import 'tailwindcss/tailwind.css'
 import Button from 'primevue/button';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 </script>
 
 <style scoped>
-h1 {
-    color: #42b983;
+.animated-bg {
+    background-size: 200% 200%;
+    animation: move-bg 6s ease infinite;
+}
+
+@keyframes move-bg {
+    0% {
+        background-position: 0% 50%;
+    }
+
+    50% {
+        background-position: 100% 50%;
+    }
+
+    100% {
+        background-position: 0% 50%;
+    }
 }
 </style>
