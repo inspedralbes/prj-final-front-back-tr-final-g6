b655c18153bc8ee85877ecfc5d61f863e69e6b77
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2025-01-23 10:47:34 +0100

    add style button login

--- front/nuxt/components/LoginForm.vue
+++ front/nuxt/components/LoginForm.vue
@@ -24,7 +24,7 @@
                     </div>
                     <div class="flex justify-center">
                         <button type="submit"
-                            class="w-[40%] text-white bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            class="w-[40%] text-white bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 border border-black">
                             Iniciar Sesión
                         </button>
                     </div>
