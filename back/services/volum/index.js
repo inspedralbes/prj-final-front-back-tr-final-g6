e5ba5b628d764e632310b890e73efb3da5447c66
a23marclacas <marticlave25@gmail.com>
2025-01-24 10:36:00 +0100

    insertar dades a mongo

--- back/services/volum/index.js
+++ back/services/volum/index.js
@@ -1,13 +1,34 @@
+import { MongoClient } from 'mongodb';
+
 // Funció per simular la lectura del volum en decibels
 function getVolumeInDecibels() {
     // Simulació d'un valor de volum en decibels
     return Math.floor(Math.random() * 100);
 }
 
-// Funció per registrar el volum cada 10 segons
-function logVolume() {
+// Connexió a MongoDB
+const url = 'mongodb+srv://marti:marti@cluster0.b6lov.mongodb.net/';
+const dbName = 'projecte_final';
+const client = new MongoClient(url);
+
+async function logVolume() {
     const volume = getVolumeInDecibels();
+    const date = new Date().toISOString();
+
     console.log(`El volum actual és de ${volume} dB`);
+
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('volum');
+
+        const result = await collection.insertOne({ volume, date });
+        console.log(`Dades inserides amb l'ID: ${result.insertedId}`);
+    } catch (error) {
+        console.error('Error inserint les dades a MongoDB:', error);
+    } finally {
+        await client.close();
+    }
 }
 
 // Configurar l'interval per executar la funció cada 10 segons
