802de246640917d1d415c19fc551f0a931d0ed9d
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2025-01-31 09:48:18 +0100

    fix error .env y dependencias

--- back/index.js
+++ back/index.js
@@ -25,10 +25,10 @@ const services = [];
 dotenv.config();
 
 const connexioBD = mysql2.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 function connectToDB() {
