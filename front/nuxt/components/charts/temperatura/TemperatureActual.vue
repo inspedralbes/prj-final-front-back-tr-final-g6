660eb2ee472dd6f17e1cada2b22e44ac3336971b
a23marclacas <156185475+a23marclacas@users.noreply.github.com>
2025-05-13 09:04:51 +0200

    prova

--- front/nuxt/components/charts/temperatura/TemperatureActual.vue
+++ front/nuxt/components/charts/temperatura/TemperatureActual.vue
@@ -161,7 +161,7 @@ onMounted(() => {
     try {
         const socketUrl = getBaseUrl();
         console.log('Connecting to socket at:', socketUrl);
-        socket.value = io(socketUrl, 
+        socket.value = io(socketUrl,  
         {
             path: '/back/socket.io',
             transports: ['websocket', 'polling'],
