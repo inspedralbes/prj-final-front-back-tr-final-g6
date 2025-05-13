6564eff26e272d64405b4f78a1846ba4055daea6
a23marclacas <156185475+a23marclacas@users.noreply.github.com>
2025-05-13 09:17:44 +0200

    ruta de socket?

--- front/nuxt/components/charts/temperatura/TemperatureActual.vue
+++ front/nuxt/components/charts/temperatura/TemperatureActual.vue
@@ -163,7 +163,7 @@ onMounted(() => {
         console.log('Connecting to socket at:', socketUrl);
         socket.value = io(socketUrl,  
         {
-            path: '/back/socket.io',
+            path: '/back/back/socket.io',
             transports: ['websocket', 'polling'],
             reconnectionAttempts: 5,
             reconnectionDelay: 1000
